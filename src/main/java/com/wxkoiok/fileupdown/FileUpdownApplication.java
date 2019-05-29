8854aa97835895d93dbf94b236b1d1e6634504a6
wuxiang <123456789>
2019-05-29 15:57:48 +0800

    项目切换

---
+++ src/main/java/com/wxkoiok/fileupdown/FileUpdownApplication.java
@@ -0,0 +1,13 @@
+package com.wxkoiok.fileupdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUpdownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUpdownApplication.class, args);
+    }
+
+}
